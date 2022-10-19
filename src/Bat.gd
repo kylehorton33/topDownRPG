aa4012eb49fc4b135dfced09c60e65b530ed2558
Kyle Horton <kylehorton33@gmail.com>
2022-10-19 12:57:12 -0400

    P21: implement bat wander

--- src/Bat.gd
+++ src/Bat.gd
@@ -5,6 +5,7 @@ const EnemyDeathEffect = preload("res://src/EnemyDeathEffect.tscn")
 export var ACCELERATION = 300
 export var MAX_SPEED = 50
 export var FRICTION = 200
+export var WANDER_TARGET_RANGE = 4
 
 enum { IDLE, WANDER, CHASE }
 
@@ -18,6 +19,10 @@ onready var stats = $Stats
 onready var player_detection_zone = $PlayerDetectionZone
 onready var hurtbox = $Hurtbox
 onready var soft_collision = $SoftCollision
+onready var wander_controller = $WanderController
+
+func _ready():
+	state = pick_random_state([IDLE, WANDER])
 
 func _physics_process(delta):
 	knockback = knockback.move_toward(Vector2.ZERO, FRICTION * delta)
@@ -27,11 +32,28 @@ func _physics_process(delta):
 		IDLE: 
 			velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 			seek_player()
-		WANDER: pass
+			
+			if wander_controller.get_time_left() == 0:
+				state = pick_random_state([IDLE, WANDER])
+				wander_controller.start_wander_timer(rand_range(1, 3))
+		WANDER:
+			seek_player()
+			
+			if wander_controller.get_time_left() == 0:
+				state = pick_random_state([IDLE, WANDER])
+				wander_controller.start_wander_timer(rand_range(1, 3))
+				
+			var direction = global_position.direction_to(wander_controller.target_position)
+			velocity = velocity.move_toward(direction * MAX_SPEED, ACCELERATION * delta)
+			
+			if global_position.distance_to(wander_controller.target_position) <= WANDER_TARGET_RANGE:
+				state = pick_random_state([IDLE, WANDER])
+				wander_controller.start_wander_timer(rand_range(1, 3))
+			
 		CHASE:
 			var player = player_detection_zone.player
 			if player != null:
-				var direction = (player.global_position - global_position).normalized()
+				var direction = global_position.direction_to(player.global_position)
 				velocity = velocity.move_toward(direction * MAX_SPEED, ACCELERATION * delta)
 			else:
 				state = IDLE
@@ -44,6 +66,10 @@ func _physics_process(delta):
 func seek_player():
 	if player_detection_zone.can_see_player():
 		state = CHASE
+		
+func pick_random_state(state_list):
+	state_list.shuffle()
+	return state_list.pop_front()
 
 func _on_Hurtbox_area_entered(area):
 	stats.health -= area.damage
